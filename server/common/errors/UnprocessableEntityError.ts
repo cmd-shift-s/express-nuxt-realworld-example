c897a7d3bab4ea22e20aaff8f4e5505cdebb982e
Gz <gz.us.to@gmail.com>
2019-03-07 11:45:43 +0900

    chore(error): remove spaces from name

--- server/common/errors/UnprocessableEntityError.ts
+++ server/common/errors/UnprocessableEntityError.ts
@@ -3,7 +3,7 @@ import { HttpError } from 'routing-controllers'
  * Exception for 422 HTTP error.
  */
 export class UnprocessableEntityError extends HttpError {
-  name: string = 'Unprocessable Entity Error'
+  name: string = 'UnprocessableEntityError'
   constructor(message?: string) {
     super(422, message)
   }
