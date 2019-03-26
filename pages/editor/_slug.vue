649f43fa8c0535051023394ab65e4e58efbc9854
Gz <gz.us.to@gmail.com>
2019-03-27 03:53:01 +0900

    page(editor): implements features
    - apply articles api
    - manage tagList
    - remove index.vue

--- pages/editor/_slug.vue
+++ pages/editor/_slug.vue
@@ -3,21 +3,27 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-10 offset-md-1 col-xs-12">
-          <form>
+          <form @submit.prevent="publish($event)">
             <fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Article Title">
+                <input v-model="articleForm.title" type="text" class="form-control form-control-lg" placeholder="Article Title">
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="What's this article about?">
+                <input v-model="articleForm.description" type="text" class="form-control" placeholder="What's this article about?">
               </fieldset>
               <fieldset class="form-group">
-                <textarea class="form-control" rows="8" placeholder="Write your article (in markdown)" />
+                <textarea v-model="articleForm.body" class="form-control" rows="8" placeholder="Write your article (in markdown)" />
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter tags"><div class="tag-list" />
+                <input v-model="inputTag" type="text" class="form-control" placeholder="Enter tags" @keydown.enter.prevent="addTag(inputTag)">
+                <div class="tag-list">
+                  <span v-for="(tag, index) of articleForm.tagList" :key="index" class="tag-default tag-pill">
+                    <i class="ion-close-round" @click="removeTag(index)" />
+                    {{ tag }}
+                  </span>
+                </div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+              <button class="btn btn-lg pull-xs-right btn-primary" type="submit">
                 Publish Article
               </button>
             </fieldset>
@@ -29,11 +35,52 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
+import { Article, ArticleFormData } from '~/models'
+import { getFieldsValue } from '~/plugins/utils'
 
 @Component({
-  middleware: 'auth'
+  middleware: 'auth',
+  async asyncData({ app, params }) {
+    if (params.slug) {
+      const { article } = await app.$axios.$get(`articles/${params.slug}`)
+      return { articleForm: article }
+    }
+  }
 })
 export default class EditorPage extends Vue {
+  articleForm: ArticleFormData = {
+    title: '',
+    description: '',
+    body: '',
+    tagList: []
+  }
+
+  inputTag: string = ''
+
+  async publish() {
+    this.$store.commit('clearError')
+
+    try {
+      const res = await this.$axios.$post('articles', { article: this.articleForm })
+      this.$router.push('/')
+    } catch (e) {
+      this.$store.commit('pushError', e.response.data)
+    }
+  }
+
+  addTag(tag: string) {
+    this.inputTag = ''
+
+    if (this.articleForm.tagList.includes(tag)) {
+      return
+    }
+
+    this.articleForm.tagList.push(tag)
+  }
+
+  removeTag(index: number) {
+    this.articleForm.tagList.splice(index, 1)
+  }
 }
 </script>
