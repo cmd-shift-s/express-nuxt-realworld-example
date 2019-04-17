c8f5e2420055ab79179dc5507cadf270d9522ff1
Gz <gz.us.to@gmail.com>
2019-04-18 02:57:36 +0900

    page(profile): apply to api params
    show loading message and empty message

--- pages/profile/_username/index.vue
+++ pages/profile/_username/index.vue
@@ -1,26 +1,52 @@
 <template>
-  <div>
+  <div v-if="loadingArticle">
+    Loading articles...
+  </div>
+
+  <div v-else-if="articleCount === 0">
+    No articles are here... yet.
+  </div>
+
+  <div v-else>
     <article-preview v-for="(article, i) of articles" :key="i" :article="article" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 import ArticlePreview from '~/components/ArticlePreview.vue'
+import { ArticleSearchParams, Article } from '~/models'
 
 @Component({
-  async asyncData({ app }) {
-    const { articles, articleCount } = await app.$axios
-      .$get('articles', { params: { limit: 10 } })
-    return {
-      articles,
-      articleCount
-    }
-  },
   components: {
     ArticlePreview
   }
 })
 export default class ProfileArticlePage extends Vue {
+  loadingArticle: boolean = false
+  articles: Article[] = []
+  articleCount: number = 0
+
+  beforeMount() {
+    this.loadArticles()
+  }
+
+  get username() {
+    return this.$route.params.username
+  }
+
+  get articleParams(): ArticleSearchParams {
+    return {
+      author: this.username
+    }
+  }
+
+  async loadArticles() {
+    this.loadingArticle = true
+    const { articles, articleCount } = await this.$axios.$get('articles', { params: this.articleParams })
+    this.loadingArticle = false
+    this.articles = articles
+    this.articleCount = articleCount
+  }
 }
 </script>
