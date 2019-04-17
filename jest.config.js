42363e3f53228cd54c553c39751e48894a859cae
Gz <gz.us.to@gmail.com>
2019-04-18 02:33:08 +0900

    config(jest): remove babel-jest transform
    bable-jest가 migration 파일들을 컴파일하면서 에러가 발생해서 제거함

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   testRegex: '(test|spec)\\.tsx?$',
   transform: {
-    '^.+\\.js$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest',
     '.*\\.(vue)$': 'vue-jest'
   }
